3fd6ec3cf8e6bfb8abe858e2b502b04470023129
tomaszociepa <tom.ociepa@gmail.com>
2019-11-12 07:18:34 +0100

    first commit

---
+++ src/main/java/pl/tom/weatherappspring/WeatherAppSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.tom.weatherappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/tom/weatherappspring/WeatherAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tom.weatherappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
