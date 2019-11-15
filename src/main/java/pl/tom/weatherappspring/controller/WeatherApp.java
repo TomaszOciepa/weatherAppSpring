48446880107f95bd23154df2f87fdeaa386ca775
tomaszociepa <tom.ociepa@gmail.com>
2019-11-15 11:35:30 +0100

    check if rain exists

--- src/main/java/pl/tom/weatherappspring/controller/WeatherApp.java
+++ src/main/java/pl/tom/weatherappspring/controller/WeatherApp.java
@@ -20,7 +20,7 @@ public class WeatherApp {
     CurrentWeather currentWeather;
 
     @GetMapping("/app")
-    public String weatherApp(){
+    public String weatherApp() {
 
         return "index";
     }
@@ -30,16 +30,39 @@ public class WeatherApp {
         Weather weather = currentWeather.get(city);
         LocalTime sunrise = getSunrise(weather);
         LocalTime sunset = getSunset(weather);
+        String iconUrl = "http://openweathermap.org/img/wn/" + weather.getWeather().get(0).getIcon() + "@2x.png";
 
-        String iconUrl = "http://openweathermap.org/img/wn/"+weather.getWeather().get(0).getIcon()+"@2x.png";
+        double rain = getRain(weather);
 
-
-        model.addAttribute("weather",weather);
+        model.addAttribute("weather", weather);
         model.addAttribute("sunrise", sunrise);
         model.addAttribute("sunset", sunset);
         model.addAttribute("iconUrl", iconUrl);
+        model.addAttribute("rain", rain);
 
         return "weather";
+    }
+
+    private double getRain(Weather weather) {
+        double rain;
+        if (weather.getRain() == null){
+            rain = 0;
+        }else {
+            if (weather.getRain().get1h() != null){
+                rain = weather.getRain().get1h();
+            } else if (weather.getRain().get2h() != null){
+                rain = weather.getRain().get2h();
+            } else if (weather.getRain().get3h() != null){
+                rain = weather.getRain().get3h();
+            } else if (weather.getRain().get4h() != null){
+                rain = weather.getRain().get4h();
+            } else if (weather.getRain().get5h() != null){
+                rain = weather.getRain().get5h();
+            }else {
+                rain = 0;
+            }
+        }
+        return rain;
     }
 
     private LocalTime getSunrise(Weather weather) {
@@ -53,4 +76,5 @@ public class WeatherApp {
         LocalDateTime sunsetUnix = LocalDateTime.ofEpochSecond(seconds,0, ZoneOffset.ofTotalSeconds(weather.getTimezone()));
         return LocalTime.of(sunsetUnix.getHour(), sunsetUnix.getMinute());
     }
+
 }
