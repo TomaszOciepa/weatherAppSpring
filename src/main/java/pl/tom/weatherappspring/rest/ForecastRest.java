068b027ad70da625dfe81b3ea2fe66e3d6cf8f56
tomaszociepa <tom.ociepa@gmail.com>
2020-01-10 16:11:06 +0100

    new token

--- src/main/java/pl/tom/weatherappspring/rest/ForecastRest.java
+++ src/main/java/pl/tom/weatherappspring/rest/ForecastRest.java
@@ -13,7 +13,7 @@ import java.net.URL;
 public class ForecastRest {
 
     public ForecastWeather get(String city) throws IOException {
-        URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=metric&APPID=58fb0cad9835fc6ae2462d0a0bfa99c5");
+        URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=metric&APPID=5edc3ee7e769f70237953bb6d7580423");
         ForecastWeather weather = new ForecastWeather();
 
         try {
